39ca81b79f3dca3dfeb45f0cbbd2c9852245c8c3
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2022-11-03 16:09:28 +0800

    added login function

--- FinalProjectDemo/src/app/rest/controllers/UserController.java
+++ FinalProjectDemo/src/app/rest/controllers/UserController.java
@@ -1,8 +1,14 @@
 package app.rest.controllers;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/user")
 public class UserController {
-
+	
+	@GET
+	@Path("/login")
+	public String login() {
+		return "test";
+	}
 }

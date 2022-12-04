39c7b402ca9e479ee5925d5d4445777e75a5a66c
= <markkevin.ongyiu@gmail.com>
2022-12-05 00:02:59 +0800

    Finished reviews for stalls

--- AFD Sandbox/js/reviewPopulate.js
+++ AFD Sandbox/js/reviewPopulate.js
@@ -1,32 +1,106 @@
-var firstRev = {
-    name: "Naik Oladive",
-    date: "June 35, 2021",
-    stallName: "Random Stall Name",
-    rating: 4,
-    reviewBody: "This is my review body hello world",
-    reviewId: 1
-}
 
-var secondRev = {
-    name: "Naik Oladive 1",
-    date: "January 13, 2131",
-    stallName: "Random Stall Name 1",
-    rating: 5,
-    reviewBody: "This is my review body hello world",
-    reviewId: 2
-}
+let allReviews;
 
-var thirdRev = {
-    name: "Naik Oladive 2",
-    date: "March 16, 2001",
-    stallName: "Random Stall Name 1",
-    rating: 1,
-    reviewBody: "This is my review body hello world",
-    reviewId: 3
-}
-
-reviewList = [firstRev, secondRev, thirdRev]
 const reviewContainer = document.getElementById("review-container")
+
+async function loadData() {
+    let stallData = await getData(`${BASE_URL}stall/list`)
+    let userData = await getData(`${BASE_URL}user/list`)
+    let reviewData = await getData(`${BASE_URL}review/list`)
+
+    let reviewList = await Promise.all(reviewData.map(async (review) => {
+        for (let stall of stallData) {
+            if (stall.stallId == review.stallId) {
+                review['stallName'] = stall.stallName;
+            }
+        }
+
+        for (let user of userData) {
+            if (user.userId == review.userId) {
+                review['userName'] = user.userName;
+            }
+        }
+
+        return review
+    }))
+
+    return reviewList;
+}
+
+async function updateReviews() {
+    // remove all children of container
+    reviewContainer.innerHTML = "";
+    let stallHeader = document.getElementById('specific-stall')
+    if (stallHeader) stallHeader.setAttribute("hidden", "")
+
+    searchSelect = document.getElementById("search-select")
+    searchInput = document.getElementById("search-input")
+
+    // output all reviews if search input is empty or if no search type is selected
+    if (searchInput.value == "" || searchSelect.value == "none") {
+        return allReviews
+    } 
+
+    // handle this later
+    if (searchSelect.value == 'user') {
+        return allReviews
+    }
+
+    // Attach image and location
+    let imageData = await getData(`${BASE_URL}image/list`)
+    let locationData = await getData(`${BASE_URL}location/list`)
+    let stallData = await getData(`${BASE_URL}stall/list`)
+
+    let currStall;
+    for (let stall of stallData) {
+        if (stall.stallName != searchInput.value) continue
+        currStall = {}
+        let reviewData = await getData(`${BASE_URL}review/stall`, {stallName: stall.stallName})
+
+        // average ratings
+        let totalRating = 0;
+        let numRatings = reviewData.length
+        for (let review of reviewData) {
+            totalRating += review.rating
+        }
+        let avgRating = (numRatings == 0 ? 'N/A': totalRating/numRatings)
+        currStall['rating'] = avgRating
+
+        // get location name
+        for (let loc of locationData) {
+            if (stall.locationId == loc.locationId) {
+                currStall['locationName'] = loc.locationName;
+            }
+        }
+
+        currStall['imagePath'] = 'images/stallpic.png'
+        // get image path
+        for (let img of imageData) {
+            if (stall.imageId == img.imageId) {
+                currStall['imagePath'] = `../FinalProjectDemo/images/${img.fileName}`;
+            }
+        }
+
+        currStall['stallName'] = stall.stallName;
+        currStall['description'] = stall.description;
+    }
+
+    if (currStall) {
+        stallHeader.removeAttribute("hidden");
+        document.getElementById('specific-stall-img').src = currStall.imagePath
+        document.getElementById("specific-stall-name").textContent = currStall.stallName
+        document.getElementById("rating-double").textContent = (currStall.rating == 'N/A' ? currStall.rating : `${currStall.rating.toFixed(1)} / 5.0`)
+        document.getElementById("stall--location").textContent = currStall.locationName
+        document.getElementById("stall--description").textContent = currStall.description
+    
+        let filteredReviews = allReviews.filter((review) => (review.stallName == searchInput.value))
+        return filteredReviews
+    }
+
+    
+
+    return allReviews
+}
 
 function createReviews(revList) {
     for (review of revList) {
@@ -49,11 +123,11 @@ function createReviews(revList) {
         // review details
         var reviewerName = document.createElement("div")
         reviewerName.classList.add("reviewer-name")
-        reviewerName.textContent = review.name
+        reviewerName.textContent = review.userName
         reviewDetails.appendChild(reviewerName)
         var reviewDate = document.createElement("div")
         reviewDate.classList.add("review-date")
-        reviewDate.textContent = review.date
+        reviewDate.textContent = review.reviewDate
         reviewDetails.appendChild(reviewDate)
         var reviewStallName = document.createElement("div")
         reviewStallName.classList.add("review-stall-name")
@@ -81,20 +155,35 @@ function createReviews(revList) {
     }
 }
 
-createReviews(reviewList)
+function init() {
+    const stall = document.getElementById("specific-stall")
+    stall.setAttribute("hidden", "");
 
-sampleStall = {
-    name: "some name here",
-    rating: 4.7,
-    location: "Gonzaga Hall",
-    description: "some description here"
+    // Load review data
+    loadData()
+        .then((data) => {
+            createReviews(data)  
+            allReviews = data
+        
+            // add listener to search input
+            // searchInput = document.getElementById("search-input")
+            // searchInput.addEventListener("input", updateStallCards);
+            //console.log(data)
+        })
+        .catch((error) => {
+            console.log(error.message)
+        })
+
+    // Set up search button
+    document.getElementById("search-btn").addEventListener('click', () => {
+        updateReviews()
+            .then((data) => {
+                createReviews(data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    });
 }
 
-function supplySpecificStallDetails(stall) {
-    document.getElementById("specific-stall-name").textContent = stall.name
-    document.getElementById("rating-double").textContent = stall.rating + " / 5.0"
-    document.getElementById("specific-stall-location").textContent += stall.location
-    document.getElementById("specific-stall-description").textContent += stall.description
-}
-
-supplySpecificStallDetails(sampleStall)
+init()
